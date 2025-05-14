1d2163a66fcc83cdcec399edbd06763727481822
Valentyn Zozulia <valikobird@protonmail.com>
2025-05-14 11:47:05 +0200

    Implement Login page layout

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,44 @@
+import { Form, Link } from "react-router-dom";
+import { FormInput, SubmitBtn } from "../components/index.jsx";
+
 const Login = () => {
-  return <h2 className="text-xl">Login</h2>;
+  return (
+    <section className="h-screen grid place-items-center">
+      <Form
+        method="post"
+        className="card w-96 p-8 bg-base-100 shadow-lg flex flex-col gap-y-4"
+      >
+        <h4 className="text-center text-3xl font-bold">Login</h4>
+        <FormInput
+          type="email"
+          label="email"
+          name="identifier"
+          defaultValue="text@text.com"
+        />
+        <FormInput
+          type="password"
+          label="password"
+          name="password"
+          defaultValue="secret"
+        />
+        <div className="mt-3">
+          <SubmitBtn text="login" />
+        </div>
+        <button type="button" className="btn btn-secondary btn-block uppercase">
+          guest user
+        </button>
+        <p className="text-center">
+          Not a member yet?
+          <Link
+            to="/register"
+            className="ml-2 link link-hover link-primary capitalize"
+          >
+            register
+          </Link>
+        </p>
+      </Form>
+    </section>
+  );
 };
 
 export default Login;
