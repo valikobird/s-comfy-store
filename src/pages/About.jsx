70f9345765394c1639727250290dd6dd2fcfe561
Valentyn Zozulia <valikobird@protonmail.com>
2025-05-16 15:08:53 +0200

    Implement About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,26 @@
 const About = () => {
-  return <h2 className="text-4xl">About</h2>;
+  return (
+    <>
+      <div className="flex flex-wrap gap-2 sm:gap-x-6 items-center justify-center">
+        <h1 className="text-4xl font-bold leading-none tracking-tight sm:text-6xl">
+          We love
+        </h1>
+        <div className="stats bg-primary shadow">
+          <div className="stat">
+            <div className="stat-title text-primary-content text-4xl sm:text-6xl font-bold tracking-wide">
+              comfy
+            </div>
+          </div>
+        </div>
+      </div>
+      <p className="mt-6 text-lg leading-8 max-w-2xl mx-auto">
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+        eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
+        voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+        clita kasd gubergren, no sea takimata
+      </p>
+    </>
+  );
 };
 
 export default About;
