7d219697872d301edf603a52f08cb46725fc1cca
Valentyn Zozulia <valikobird@protonmail.com>
2025-05-21 12:36:41 +0200

    Implement product color selector

--- src/pages/SingleProduct/SingleProduct.jsx
+++ src/pages/SingleProduct/SingleProduct.jsx
@@ -1,10 +1,14 @@
 import { Link, useLoaderData } from "react-router-dom";
 import { formatPrice } from "../../utils";
+import { useState } from "react";
 
 const SingleProduct = () => {
   const { product } = useLoaderData();
-  const { image, title, price, description, company } = product.attributes;
+  const { image, title, price, description, company, colors } =
+    product.attributes;
   const dollarsAmount = formatPrice(price);
+
+  const [productColor, setProductColor] = useState(colors[0]);
 
   return (
     <section>
@@ -31,6 +35,24 @@ const SingleProduct = () => {
           </h4>
           <p className="mt-3 text-xl">{dollarsAmount}</p>
           <p className="mt-6 leading-8">{description}</p>
+          <div className="mt-6">
+            <h4 className="text-md font-medium tracking-wide capitalize">
+              colors
+            </h4>
+            <div className="mt-2">
+              {colors.map((color) => {
+                return (
+                  <button
+                    key={color}
+                    type="button"
+                    className={`badge w-6 h-6 mr-2 cursor-pointer ${color === productColor && "border-2 border-secondary"}`}
+                    style={{ backgroundColor: color }}
+                    onClick={() => setProductColor(color)}
+                  ></button>
+                );
+              })}
+            </div>
+          </div>
         </div>
       </div>
     </section>
