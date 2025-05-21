eb70ea437d410d2c8ec2332cab8369dee1d93276
Valentyn Zozulia <valikobird@protonmail.com>
2025-05-21 12:14:18 +0200

    Render base product info

--- src/pages/SingleProduct/SingleProduct.jsx
+++ src/pages/SingleProduct/SingleProduct.jsx
@@ -1,5 +1,40 @@
+import { Link, useLoaderData } from "react-router-dom";
+import { formatPrice } from "../../utils";
+
 const SingleProduct = () => {
-  return <h2 className="text-4xl">SingleProduct</h2>;
+  const { product } = useLoaderData();
+  const { image, title, price, description, company } = product.attributes;
+  const dollarsAmount = formatPrice(price);
+
+  return (
+    <section>
+      <div className="text-md breadcrumbs">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/products">Products</Link>
+          </li>
+        </ul>
+      </div>
+      <div className="mt-6 grid gap-y-8 lg:grid-cols-2 lg:gap-x-16">
+        <img
+          src={image}
+          alt={title}
+          className="w-96 h-96 object-cover rounded-lg lg:w-full"
+        />
+        <div>
+          <h1 className="capitalize text-3xl font-semibold">{title}</h1>
+          <h4 className="text-xl text-secondary font-semibold mt-2">
+            {company}
+          </h4>
+          <p className="mt-3 text-xl">{dollarsAmount}</p>
+          <p className="mt-6 leading-8">{description}</p>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default SingleProduct;
