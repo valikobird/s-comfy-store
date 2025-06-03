6f2e2662c4a82842325f6b0c8b5e9872f161c6cf
Valentyn Zozulia <valikobird@protonmail.com>
2025-06-03 14:38:17 +0200

    Implement Guest user login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,7 +1,29 @@
-import { Form, Link } from "react-router-dom";
+import { Form, Link, useNavigate } from "react-router-dom";
 import { FormInput, SubmitBtn } from "../../components";
+import { useDispatch } from "react-redux";
+import { toast } from "react-toastify";
+import { customFetch } from "../../utils";
+import { loginUser } from "../../features/user/userSlice";
 
 const Login = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const loginAsGuest = async () => {
+    try {
+      const response = await customFetch.post("/auth/local", {
+        identifier: "test@test.com",
+        password: "secret",
+      });
+      dispatch(loginUser(response.data));
+      toast.success("Welcome Guest user");
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+      toast.error("Guest user login error. Please try again.");
+    }
+  };
+
   return (
     <section className="h-screen grid place-items-center">
       <Form
@@ -9,22 +31,16 @@ const Login = () => {
         className="card w-96 p-8 bg-base-100 shadow-lg flex flex-col gap-y-4"
       >
         <h4 className="text-center text-3xl font-bold">Login</h4>
-        <FormInput
-          type="email"
-          label="email"
-          name="identifier"
-          defaultValue="text@text.com"
-        />
-        <FormInput
-          type="password"
-          label="password"
-          name="password"
-          defaultValue="secret"
-        />
+        <FormInput type="email" label="email" name="identifier" />
+        <FormInput type="password" label="password" name="password" />
         <div className="mt-3">
           <SubmitBtn text="login" />
         </div>
-        <button type="button" className="btn btn-secondary btn-block uppercase">
+        <button
+          type="button"
+          className="btn btn-secondary btn-block uppercase"
+          onClick={loginAsGuest}
+        >
           guest user
         </button>
         <p className="text-center">
