ed2bd5f3cf58ee0dc27900af7ab0a98e5ce59473
Valentyn Zozulia <valikobird@protonmail.com>
2025-06-04 14:05:23 +0200

    Add and configure ReactQuery

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import {
   About,
   Cart,
@@ -25,6 +27,14 @@ import { action as loginAction } from "./pages/Login/actions";
 import { action as checkoutAction } from "./components/CheckoutForm/actions";
 
 import { store } from "./store";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5,
+    },
+  },
+});
 
 const router = createBrowserRouter([
   {
@@ -86,7 +96,12 @@ const router = createBrowserRouter([
 ]);
 
 const App = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 };
 
 export default App;
